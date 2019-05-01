c797323e5e75b23800152950099526f76fba3103
GarrettSorel <garrettsorel@gmail.com>
2019-04-30 22:18:37 -0400

    solved

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(f)
+   t = (f.to_f - 32) * 5/9
+   return t
+end
+
+def ctof(c)
+  t = (c.to_f * 9/5) + 32
+  return t
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add(num_1, num_2)
+  num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+  num_1 - num_2
+end
+
+def sum(numbers)
+  numbers.sum
+end
+
+def multiply(numbers)
+  numbers.inject(:*)
+end
+
+def power(num_1, num_2)
+  num_1 ** num_2
+end
+
+def factorial(number)
+  factorial_total = []
+
+  if number == 0
+    return 0
+  end
+
+  if number > 0
+    while number > 0 do
+      factorial_total.push(number)
+      number - 1
+    end
+  end
+
+  return factorial_total.inject(:*)
+end
