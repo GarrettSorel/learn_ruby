850ef670fea09a8e0756dcb5ae4ca1071a149dfd
GarrettSorel <garrettsorel@gmail.com>
2019-05-02 16:35:47 -0400

    solved

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,19 +1,22 @@
 def translate(str)
-  transform = str.split('')
-  vowels = /[aeiou]/
-  consonants = /[bcdfghjklmnpqrstvwxyz]/
+  transform = str.split(" ")
+  vowels = /^[aeiou]/
+  consonants = /[bcdfghjklmnprstvwxyz]/
+  pig_latin = []
 
-  if str.start_with?(vowels)
-    return str + "ay"
-
-  elsif transform[0] != vowels && transform[1].match?(vowels)
-    transform.rotate!
-    new_str = transform.join('')
-    return new_str + "ay"
-
-  elsif transform[0] != vowels && transform[1].match(consonants)
-    transform.rotate!(2)
-    new_str = transform.join('')
-    return new_str + "ay"
+  transform.map do |s|
+    if vowels.match(s)
+      pig_latin.push(s + "ay")
+    elsif s.start_with?(consonants) && s[1].match?(vowels)
+      pig_latin.push(s[1..-1] + s[0] + "ay")
+    elsif s.start_with?(consonants) && s[1].match?(consonants) && s[2].match?(vowels)
+      pig_latin.push(s[2..-1] + s[0..1] + "ay")
+    elsif s.start_with?(consonants) && s[1].match?(consonants) && s[2].match?(consonants)
+      pig_latin.push(s[3..-1] + s[0..2] + "ay")
+    elsif s.start_with?(/[qu]/)
+      pig_latin.push(s[2..-1] + "quay")
+    end
   end
+
+  pig_latin.join(" ")
 end
