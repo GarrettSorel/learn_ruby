8c7d21fb5d2b70207519b42e56e044c675733e33
GarrettSorel <garrettsorel@gmail.com>
2019-05-02 13:20:45 -0400

    work on pig latin project

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+def translate(str)
+  transform = str.split('')
+  vowels = /[aeiou]/
+  consonants = /[bcdfghjklmnpqrstvwxyz]/
+
+  if str.start_with?(vowels)
+    return str + "ay"
+
+  elsif transform[0] != vowels && transform[1].match?(vowels)
+    transform.rotate!
+    new_str = transform.join('')
+    return new_str + "ay"
+
+  elsif transform[0] != vowels && transform[1].match(consonants)
+    transform.rotate!(2)
+    new_str = transform.join('')
+    return new_str + "ay"
+  end
+end
