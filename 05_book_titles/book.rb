5926d4c1ca89e2c1e8941370aefdee71cf879c51
GarrettSorel <garrettsorel@gmail.com>
2019-05-03 14:19:17 -0400

    solved

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title
+    fix_title = @title.split(" ")
+    ignore_words = %w(an in for and of a the)
+
+    fix_title.map do |word|
+      if ignore_words.include?(word)
+        word.downcase!
+      else
+        word.capitalize!
+      end
+    end
+    
+    fix_title[0].capitalize!
+    @title = fix_title.join(" ")
+  end
 end
